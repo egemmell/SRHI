cf14feba179907398341620553035cd6df1ec9f4
egemmell <emilygemmell@gmail.com>
2025-11-11 14:08:35 -0800

    Update lung cancer data cleaning script.

--- src/data/clean_health_data.R
+++ src/data/clean_health_data.R
@@ -10,19 +10,19 @@ library(RColorBrewer)
 library(tidyverse)
 
 # load county and census tract shapefiles
-counties <- st_read("BREATHE HEALTH/data/raw/counties_and_centroids.shp")
+counties <- st_read("data/raw/counties_and_centroids.shp")
 counties <- st_as_sf(counties, crs = 4326)
 counties <- counties[, 2]
 
-ct_tract <- st_read("BREATHE HEALTH/data/raw/ct_tract.shp")
+ct_tract <- st_read("data/raw/ct_tract.shp")
 
 # load mortality data, limit to needed columns and rows
 
-acm <- read_csv("BREATHE HEALTH/data/raw/IHME_USA_COD_COUNTY_RACE_ETHN_2000_2019_MX_2019_ALL_BOTH_Y2023M06D12.CSV")
+acm <- read_csv("data/raw/IHME_USA_COD_COUNTY_RACE_ETHN_2000_2019_MX_2019_ALL_BOTH_Y2023M06D12.CSV")
 
 acm <- acm[grep("California", acm$location_name), c(3:5,7,9,11,13,14,16:19)]
 
-#standardize column names and limit to census tracts in the SFBA of interest
+#standardize column names and limit to counties in the SFBA 
 
 acm$location_name <- sub(" County (California)", "", acm$location_name, fixed = TRUE)
 
@@ -75,13 +75,13 @@ colnames(acm) <- c("location_name",
 acm$fips <- str_pad(acm$fips, width = 5, side = "left", pad = "0")
 
 #write csv files to clean data folder
-write_csv(acm, "BREATHE HEALTH/data/processed/all_cause_mortality_2019.csv", append = FALSE)
+write_csv(acm, "data/processed/ac_mortality_county_2019.csv", append = FALSE)
 
 
 #############################################################################
 # Cause-specific mortality: CVD 2014
 
-ihd <- read_csv("BREATHE HEALTH/data/raw/IHME_USA_COUNTY_CVD_MORTALITY_RATES_1980_2014_CALIFORNIA_Y2017M05D16.CSV")
+ihd <- read_csv("data/raw/IHME_USA_COUNTY_CVD_MORTALITY_RATES_1980_2014_CALIFORNIA_Y2017M05D16.CSV")
 
 ihd <- ihd[ihd$year_id == 2014, ]
 ihd <- ihd[ihd$cause_name == "Ischemic heart disease", ]
@@ -132,12 +132,12 @@ colnames(ihd) <- c("location_name",
 ihd$fips <- str_pad(ihd$fips, width = 5, side = "left", pad = "0")
 
 #write csv files to clean data folder
-write_csv(ihd, "BREATHE HEALTH/data/processed/cvd_mortality_2014.csv", append = FALSE)
+write_csv(ihd, "data/processed/ihd_mortality_county_2014.csv", append = FALSE)
 
 
 #############################################################################
 # Cause-specific mortality Lung cancer 2014
-lcan <- read_csv("BREATHE HEALTH/data/raw/IHME_USA_COUNTY_CANCER_MORTALITY_RATES_1980_2014_CALIFORNIA_Y2017M01D24.CSV")
+lcan <- read_csv("data/raw/IHME_USA_COUNTY_CANCER_MORTALITY_RATES_1980_2014_CALIFORNIA_Y2017M01D24.CSV")
 
 lcan <- lcan[lcan$year_id == 2014, ]
 
@@ -166,23 +166,23 @@ colnames(lcan) <- c("location_name",
                    "lCI"
 )
 
-
+lcan$fips <- str_pad(lcan$fips, width = 5, side = "left", pad = "0")
 #write csv files to clean data folder
 
-write_csv(lcan, "BREATHE HEALTH/data/processed/lc_mortality_2014.csv")
+write_csv(lcan, "data/processed/lc_mortality_county_2014.csv")
 
 ####################################################################################################
 # Lung cancer mortality 2019
 
-lcan_b <- read_csv("BREATHE HEALTH/data/raw/IHME_USA_LUNG_CANCER_COUNTY_RACE_ETHNICITY_2000_2019_MX_2019_BOTH_Y2025M06D15.csv")
+lcan_b <- read_csv("data/raw/IHME_USA_LUNG_CANCER_COUNTY_RACE_ETHNICITY_2000_2019_MX_2019_BOTH_Y2025M06D15.csv")
 lcan_b <- lcan_b[grep("California", lcan_b$location_name), c(3:5,7,9,11,13,14,16:19)]
 lcan_b <- as.data.frame(lcan_b)
 
-lcan_f <- read_csv("BREATHE HEALTH/data/raw/IHME_USA_LUNG_CANCER_COUNTY_RACE_ETHNICITY_2000_2019_MX_2019_FEMALE_Y2025M06D15.csv")
+lcan_f <- read_csv("data/raw/IHME_USA_LUNG_CANCER_COUNTY_RACE_ETHNICITY_2000_2019_MX_2019_FEMALE_Y2025M06D15.csv")
 lcan_f <- lcan_f[grep("California", lcan_f$location_name), c(3:5,7,9,11,13,14,16:19)]
 lcan_f <- as.data.frame(lcan_f)
 
-lcan_m <- read_csv("BREATHE HEALTH/data/raw/IHME_USA_LUNG_CANCER_COUNTY_RACE_ETHNICITY_2000_2019_MX_2019_MALE_Y2025M06D15.csv")
+lcan_m <- read_csv("data/raw/IHME_USA_LUNG_CANCER_COUNTY_RACE_ETHNICITY_2000_2019_MX_2019_MALE_Y2025M06D15.csv")
 lcan_m <- lcan_m[grep("California", lcan_m$location_name), c(3:5,7,9,11,13,14,16:19)]
 lcan_m <- as.data.frame(lcan_m)
 
@@ -209,7 +209,7 @@ sfba <- c("Alameda",
 lcan <- lcan[lcan$location_name %in% sfba, ] 
 
 lcan[lcan$race_name == "AIAN", 'race_name'] <- "American Indian / Alaskan Native"
-lcan[lcan$race_name == "API", 'race_name'] <- "Asian / Pacific Islander"
+lcan[lcan$race_name == "Asian", 'race_name'] <- "Asian / Pacific Islander"
 lcan[lcan$race_name == "Latino", 'race_name'] <- "Hispanic"
 
 lcan$race_name <- factor(lcan$race_name, levels = c("American Indian / Alaskan Native", "Black", "White", "Hispanic", "Asian / Pacific Islander", "Total"))  
@@ -241,255 +241,18 @@ colnames(lcan) <- c("location_name",
 lcan$fips <- str_pad(lcan$fips, width = 5, side = "left", pad = "0")
 
 #write csv files to clean data folder
-write_csv(lcan, "BREATHE HEALTH/data/processed/lcan_mortality_2019.csv", append = FALSE)
+write_csv(lcan, "data/processed/lcan_mortality_county_2019.csv", append = FALSE)
 
-lcan_b <- lcan[lcan$sex_name == "Both", ]
-lcan_c <- lcan_b[lcan_b$race_name == "Total", ]
-lcan_d <- lcan_c[lcan_c$age_name == "Age-standardized", ]
-lcan_e <- drop_na(lcan_d)
+###################################################################
+# calculate the age-adjusted lung cancer mortality rates for census tracts, given 
+# census tract race/ethnicity demographics
 
-# lung cancer, age standardized
-my_palette <- brewer.pal(8, "Set2") 
-
-# Add more colors to this palette :
-my_palette <- colorRampPalette(my_palette)(9)
-
-# Plot it
-pie(rep(1, length(my_palette)), col = my_palette , main="") 
-
-ac_mort1 <- lcan_e %>%
-  ggplot(aes(x = location_name, y = mx)) +
-  geom_bar(aes(fill = location_name), stat = "identity", position = "dodge") +
-  guides(fill = "none") +
-  geom_errorbar(aes(ymin=lCI, ymax=uCI, group = location_name), 
-                position = position_dodge(.9), 
-                stat = "identity",
-                width = 0.25, 
-                colour="orange", 
-                alpha=0.9, 
-                linewidth=0.5) +
-  xlab("County") + # for the x axis label
-  ylab("Age-adjusted, lung cancer mortality (deaths per 100,000 pop)") + # for the y axis label
-  labs(title = "Age-adjusted, lung cancer mortality in 2019 by county") +
-  scale_fill_brewer(palette = "Set3") +
-  theme_classic()
-
-ac_mort1
-
-
-#lung cancer, age standardized, by race
-
-ac_mort1 <- lcan_b[lcan_b$age_name == "Age-standardized", ] %>%
-  ggplot(aes(x = location_name, y = mx)) +
-  geom_bar(aes(fill = race_name), stat = "identity", position = "dodge") +
-
-  geom_errorbar(aes(ymin=lCI, ymax=uCI, group = race_name), 
-                position = position_dodge(.9), 
-                stat = "identity",
-                width = 0.25, 
-                colour="orange", 
-                alpha=0.9, 
-                linewidth=0.5) +
-  xlab("County") + # for the x axis label
-  ylab("Age-adjusted, lung cancer mortality (deaths per 100,000 pop)") + # for the y axis label
-  labs(title = "Age-adjusted, lung cancer mortality in 2019, by race", fill = "Race") +
-  scale_fill_brewer(palette = "Set2") +
-
-  theme_classic()
-
-ac_mort1
-
-# LC deaths by age (all races, sexes)
-
-lcan_b <- lcan[lcan$sex_name == "Both", ]
-lcan_b <- lcan_b[lcan_b$race_name == "Total", ]
-
-#ages for which 2019 data has >0 lung cancer deaths
-lc_ages <- c("15 to 19",         
-             "20 to 24",        
-             "25 to 29",         
-             "30 to 34",         
-             "35 to 39",         
-             "40 to 44",
-             "45 to 49",         
-             "50 to 54",        
-             "55 to 59",         
-             "60 to 64",         
-             "65 to 69",         
-             "70 to 74",         
-             "75 to 79",         
-             "80 to 84",        
-             "85 plus")
-
-lcan_b <- lcan_b[lcan_b$age_name %in% lc_ages, ]
-# Classic palette BuPu, with 4 colors
-
-my_palette <- brewer.pal(8, "Set2") 
-
-# Add more colors to this palette :
-my_palette <- colorRampPalette(my_palette)(15)
-
-# Plot it
-pie(rep(1, length(my_palette)), col = my_palette , main="") 
-
-ac_mort1 <- lcan_b %>%
-  ggplot(aes(x = location_name, y = mx)) +
-  geom_bar(aes(fill = age_name), stat = "identity", position = "dodge") +
-  geom_errorbar(aes(ymin=lCI, ymax=uCI, group = age_name), 
-                position = position_dodge(.9), 
-                stat = "identity",
-                width = 0.25, 
-                colour="orange", 
-                alpha=0.9, 
-                linewidth=0.5) +
-  xlab("County") + # for the x axis label
-  ylab("Lung cancer mortality (deaths per 100,000 pop)") + # for the y axis label
-  labs(title = "Lung cancer mortality by age group, 2019", fill = "Age Group") +
-  #scale_fill_brewer(palette = my_palette) +
-  theme_classic()
-
-ac_mort1
-
-# LC deaths by sex (all races, age adjusted)
-lcan_b <- lcan[lcan$race_name == "Total", ]
-lcan_b <- drop_na(lcan_b)
-
-
-ac_mort1 <- lcan_b[lcan_b$age_name == "Age-standardized", ] %>%
-  ggplot(aes(x = location_name, y = mx)) +
-  geom_bar(aes(fill = sex_name), stat = "identity", position = "dodge") +
-  geom_errorbar(aes(ymin=lCI, ymax=uCI, group = sex_name), 
-                position = position_dodge(.9), 
-                stat = "identity",
-                width = 0.25, 
-                colour="orange", 
-                alpha=0.9, 
-                linewidth=0.5) +
-  xlab("County") + # for the x axis label
-  ylab("Age-standardized lung cancer mortality (deaths per 100,000 pop)") + # for the y axis label
-  labs(title = "Age-standardized lung cancer mortality by sex, 2019", fill = "Sex") +
-  scale_fill_brewer(palette = "Set2") +
-  theme_classic()
-
-ac_mort1
-# merge shapefiles with data
-
-all_cause <- all_cause[all_cause$race_name == "Total", ]
-
-ac_shp <- merge(counties, all_cause, by.x = "NAME", by.y = "location_name")
-
-ac_shp <- st_as_sf(ac_shp)
-#ac_shp <- st_transform(ac_shp)
-
-
-pal <- colorNumeric(
-  palette = "OrRd", domain = ac_shp$deaths,
-  na.color = "transparent"
-)
-
-leaflet(data = baseline) %>% 
-  addTiles(group = "OpenStreetMap") %>%
-  
-  addPolygons(stroke = TRUE, color = "#444444", weight = 1, smoothFactor = 0.5,
-              opacity = 1.0, fillOpacity = 0.8,
-              fillColor = ~pal(deaths),
-              highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                  bringToFront = TRUE)) %>%
-  
-  
-#      opacity = 1,
-#      values = ~ (switch(rv$dom, 
-#                  "Playability Score" = dc_ps,
-#                  "Space for play" = dc_sfp,
-#                  "Social environment" = dc_sdi,
-#                  "Traffic environment" = dc_ted,
-#                  "Natural environment" = dc_ned,
-#                  "Child-relevant destinations" = dc_crd)), 
-#      na.label = "missing data",
-#      title = paste0(as.character(rv$dom), "<br>(metro area decile)"))
+lc_cty <- read_csv("data/raw/californiahealthmaps_county_all.csv")
+lc_cty <- lc_cty[lc_cty$Cancer == "Lung", -c(3,4)]
+lc_cty <- lc_cty[lc_cty$Counties %in% sfba, ]
+lc_cty <- lc_cty[lc_cty$Years  == "05yr", ]
+lc_cty <- lc_cty[lc_cty$Sex  == "Both", ]
 
 
 
-baseline <- all_cause[all_cause$race_name == "Total",]
-baseline <- baseline[baseline$age_name == "Age-standardized", ]
 
-# merge with all_cause mortality data
-
-baseline <- merge(baseline, counties, by.x = "location_name", by.y = "NAME", all.x = TRUE)
-baseline <- st_as_sf(baseline)
-baseline <- st_transform(baseline, 4326)
-
-pal <- colorNumeric(
-  palette = "Blues", domain = baseline$deaths,
-  na.color = "transparent"
-)
-
-leaflet(data = baseline) %>% 
-  addTiles(group = "OpenStreetMap") %>%
-  
-  addPolygons(stroke = TRUE, color = "#444444", weight = 1, smoothFactor = 0.5,
-              opacity = 1.0, fillOpacity = 0.8,
-              fillColor = pal),
-              highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                  bringToFront = TRUE)) %>%
-  addLegend()
-
-%>%
-  
-  addLabelOnlyMarkers(data = centroids,
-                      lng = ~Lon, lat = ~Lat, label = ~NAME,
-                      labelOptions = labelOptions(noHide = TRUE, direction = 'top', textOnly = TRUE)) 
-
-  #addLegend(data = ps_dat,
-            #      position = "topright",
-            #      pal = ~color_ps(),
-            #      opacity = 1,
-            #      values = ~ (switch(rv$dom, 
-            #                  "Playability Score" = dc_ps,
-            #                  "Space for play" = dc_sfp,
-            #                  "Social environment" = dc_sdi,
-            #                  "Traffic environment" = dc_ted,
-            #                  "Natural environment" = dc_ned,
-            #                  "Child-relevant destinations" = dc_crd)), 
-            #      na.label = "missing data",
-            #      title = paste0(as.character(rv$dom), "<br>(metro area decile)"))
-  
-  
-                  
-# Make cvd map
-baseline <- cvd[cvd$sex == "Both",]
-baseline <- baseline[baseline$cause_name == "Ischemic heart disease", ]
-baseline <- baseline[baseline$age_name == "Age-standardized", ]
-
-# merge with all_cause mortality data
-
-baseline <- merge(baseline, counties, by.x = "location_name", by.y = "NAME", all.x = TRUE)
-baseline <- st_as_sf(baseline)
-baseline <- st_transform(baseline, 4326)
-
-pal <- colorNumeric(
-  palette = "BuGn", domain = acm$mx,
-  na.color = "transparent"
-)
-
-leaflet(data = acm) %>% 
-  addTiles(group = "OpenStreetMap") %>%
-  
-  addPolygons(data = acm,
-              stroke = TRUE, 
-              weight = 1, 
-              smoothFactor = 0.5,
-              opacity = 1.0, 
-              fillOpacity = 0.8,
-              fillColor = pal,
-              highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                  bringToFront = TRUE)) %>%
-  addLegend(data = baseline,
-            position = "topright",
-            pal = pal,
-            values = ~mx,
-            opacity = 1,
-            title = "Age-adjusted mortality <br>from IHD<br>(per 100,000 pop)"
-            )
-
-%>%
--- src/visualisation/health_data_visualizations.R
+++ src/visualisation/health_data_visualizations.R
@@ -40,7 +40,7 @@ ac_mort1 <- acm[acm$age_name == "Age-standardized", ] %>%
 ac_mort1
 
 #########################################
-# Ischemic heart disease mortality by county and sex
+# barplot Ischemic heart disease mortality by county and sex
 ihd_mort <- ihd %>%
   ggplot(aes(x = location_name, y = mx)) +
   geom_bar(aes(fill = sex_name), 
@@ -62,6 +62,7 @@ ihd_mort <- ihd %>%
 ihd_mort
 
 
+# barplot 2014 age-adjusted lung cancer mortality by sex
 lc_mort <- lcan %>%
   ggplot(aes(x = location_name, y = mx)) +
   geom_bar(aes(fill = sex_name), 
@@ -82,3 +83,224 @@ lc_mort <- lcan %>%
 
 lc_mort
 
+lcan_b <- lcan[lcan$sex_name == "Both", ]
+lcan_c <- lcan_b[lcan_b$race_name == "Total", ]
+lcan_d <- lcan_c[lcan_c$age_name == "Age-standardized", ]
+lcan_e <- drop_na(lcan_d)
+
+# lung cancer, age standardized
+my_palette <- brewer.pal(8, "Set2") 
+
+# Add more colors to this palette :
+my_palette <- colorRampPalette(my_palette)(9)
+
+# Plot it
+pie(rep(1, length(my_palette)), col = my_palette , main="") 
+
+ac_mort1 <- lcan_e %>%
+  ggplot(aes(x = location_name, y = mx)) +
+  geom_bar(aes(fill = location_name), stat = "identity", position = "dodge") +
+  guides(fill = "none") +
+  geom_errorbar(aes(ymin=lCI, ymax=uCI, group = location_name), 
+                position = position_dodge(.9), 
+                stat = "identity",
+                width = 0.25, 
+                colour="orange", 
+                alpha=0.9, 
+                linewidth=0.5) +
+  xlab("County") + # for the x axis label
+  ylab("Age-adjusted, lung cancer mortality (deaths per 100,000 pop)") + # for the y axis label
+  labs(title = "Age-adjusted, lung cancer mortality in 2019 by county") +
+  scale_fill_brewer(palette = "Set3") +
+  theme_classic()
+
+ac_mort1
+
+
+#lung cancer, age standardized, by race
+
+ac_mort1 <- lcan_b[lcan_b$age_name == "Age-standardized", ] %>%
+  ggplot(aes(x = location_name, y = mx)) +
+  geom_bar(aes(fill = race_name), stat = "identity", position = "dodge") +
+  
+  geom_errorbar(aes(ymin=lCI, ymax=uCI, group = race_name), 
+                position = position_dodge(.9), 
+                stat = "identity",
+                width = 0.25, 
+                colour="orange", 
+                alpha=0.9, 
+                linewidth=0.5) +
+  xlab("County") + # for the x axis label
+  ylab("Age-adjusted, lung cancer mortality (deaths per 100,000 pop)") + # for the y axis label
+  labs(title = "Age-adjusted, lung cancer mortality in 2019, by race", fill = "Race") +
+  scale_fill_brewer(palette = "Set2") +
+  
+  theme_classic()
+
+ac_mort1
+
+# LC deaths by age (all races, sexes)
+
+lcan_b <- lcan[lcan$sex_name == "Both", ]
+lcan_b <- lcan_b[lcan_b$race_name == "Total", ]
+
+#ages for which 2019 data has >0 lung cancer deaths
+lc_ages <- c("15 to 19",         
+             "20 to 24",        
+             "25 to 29",         
+             "30 to 34",         
+             "35 to 39",         
+             "40 to 44",
+             "45 to 49",         
+             "50 to 54",        
+             "55 to 59",         
+             "60 to 64",         
+             "65 to 69",         
+             "70 to 74",         
+             "75 to 79",         
+             "80 to 84",        
+             "85 plus")
+
+lcan_b <- lcan_b[lcan_b$age_name %in% lc_ages, ]
+# Classic palette BuPu, with 4 colors
+
+my_palette <- brewer.pal(8, "Set2") 
+
+# Add more colors to this palette :
+my_palette <- colorRampPalette(my_palette)(15)
+
+# Plot it
+pie(rep(1, length(my_palette)), col = my_palette , main="") 
+
+ac_mort1 <- lcan_b %>%
+  ggplot(aes(x = location_name, y = mx)) +
+  geom_bar(aes(fill = age_name), stat = "identity", position = "dodge") +
+  geom_errorbar(aes(ymin=lCI, ymax=uCI, group = age_name), 
+                position = position_dodge(.9), 
+                stat = "identity",
+                width = 0.25, 
+                colour="orange", 
+                alpha=0.9, 
+                linewidth=0.5) +
+  xlab("County") + # for the x axis label
+  ylab("Lung cancer mortality (deaths per 100,000 pop)") + # for the y axis label
+  labs(title = "Lung cancer mortality by age group, 2019", fill = "Age Group") +
+  #scale_fill_brewer(palette = my_palette) +
+  theme_classic()
+
+ac_mort1
+
+# LC deaths by sex (all races, age adjusted)
+lcan_b <- lcan[lcan$race_name == "Total", ]
+lcan_b <- drop_na(lcan_b)
+
+
+ac_mort1 <- lcan_b[lcan_b$age_name == "Age-standardized", ] %>%
+  ggplot(aes(x = location_name, y = mx)) +
+  geom_bar(aes(fill = sex_name), stat = "identity", position = "dodge") +
+  geom_errorbar(aes(ymin=lCI, ymax=uCI, group = sex_name), 
+                position = position_dodge(.9), 
+                stat = "identity",
+                width = 0.25, 
+                colour="orange", 
+                alpha=0.9, 
+                linewidth=0.5) +
+  xlab("County") + # for the x axis label
+  ylab("Age-standardized lung cancer mortality (deaths per 100,000 pop)") + # for the y axis label
+  labs(title = "Age-standardized lung cancer mortality by sex, 2019", fill = "Sex") +
+  scale_fill_brewer(palette = "Set2") +
+  theme_classic()
+
+ac_mort1
+# merge shapefiles with data
+
+all_cause <- all_cause[all_cause$race_name == "Total", ]
+
+ac_shp <- merge(counties, all_cause, by.x = "NAME", by.y = "location_name")
+
+ac_shp <- st_as_sf(ac_shp)
+#ac_shp <- st_transform(ac_shp)
+
+
+pal <- colorNumeric(
+  palette = "OrRd", domain = ac_shp$deaths,
+  na.color = "transparent"
+)
+
+leaflet(data = baseline) %>% 
+  addTiles(group = "OpenStreetMap") %>%
+  
+  addPolygons(stroke = TRUE, color = "#444444", weight = 1, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.8,
+              fillColor = ~pal(deaths),
+              highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                  bringToFront = TRUE)) %>%
+
+  
+  baseline <- all_cause[all_cause$race_name == "Total",]
+baseline <- baseline[baseline$age_name == "Age-standardized", ]
+
+# merge with all_cause mortality data
+
+baseline <- merge(baseline, counties, by.x = "location_name", by.y = "NAME", all.x = TRUE)
+baseline <- st_as_sf(baseline)
+baseline <- st_transform(baseline, 4326)
+
+pal <- colorNumeric(
+  palette = "Blues", domain = baseline$deaths,
+  na.color = "transparent"
+)
+
+leaflet(data = baseline) %>% 
+  addTiles(group = "OpenStreetMap") %>%
+  
+  addPolygons(stroke = TRUE, color = "#444444", weight = 1, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.8,
+              fillColor = pal),
+              highlightOptions = highlightOptions(color = "white", weight = 2,
+                                    bringToFront = TRUE)) %>%
+  addLegend() %>%
+
+  
+  addLabelOnlyMarkers(data = centroids,
+                      lng = ~Lon, lat = ~Lat, label = ~NAME,
+                      labelOptions = labelOptions(noHide = TRUE, direction = 'top', textOnly = TRUE)) 
+
+
+# Make cvd map
+baseline <- cvd[cvd$sex == "Both",]
+baseline <- baseline[baseline$cause_name == "Ischemic heart disease", ]
+baseline <- baseline[baseline$age_name == "Age-standardized", ]
+
+# merge with all_cause mortality data
+
+baseline <- merge(baseline, counties, by.x = "location_name", by.y = "NAME", all.x = TRUE)
+baseline <- st_as_sf(baseline)
+baseline <- st_transform(baseline, 4326)
+
+pal <- colorNumeric(
+  palette = "BuGn", domain = acm$mx,
+  na.color = "transparent"
+)
+
+leaflet(data = acm) %>% 
+  addTiles(group = "OpenStreetMap") %>%
+  
+  addPolygons(data = acm,
+              stroke = TRUE, 
+              weight = 1, 
+              smoothFactor = 0.5,
+              opacity = 1.0, 
+              fillOpacity = 0.8,
+              fillColor = pal,
+              highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                  bringToFront = TRUE)) %>%
+  addLegend(data = baseline,
+            position = "topright",
+            pal = pal,
+            values = ~mx,
+            opacity = 1,
+            title = "Age-adjusted mortality <br>from IHD<br>(per 100,000 pop)"
+  )
+
+%>%
